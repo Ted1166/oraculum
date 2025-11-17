4d05a37abce26e3b3c3a41c934a00fbe714e4eab
Ted1166 <ogolated00@gmail.com>
2025-11-17 18:52:36 +0300

    pulsepool

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -31,7 +31,7 @@ export const Header = () => {
               <Zap className="w-5 h-5 text-background" />
             </div>
             <span className="font-bold text-xl bg-gradient-primary bg-clip-text text-transparent">
-              Predict & Fund
+              PulsePool
             </span>
           </Link>
 
